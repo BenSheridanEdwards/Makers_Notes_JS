03f81a23a32b6eba362c3c51ea18fb1037096545
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-08 14:52:41 +0000

    Insert note list HTML into app

---
+++ spec/note-controller-spec.js
@@ -0,0 +1,17 @@
+describe('Note Controller', () => {
+  it('Instantates with a note list object', () => {
+    var noteList = new NoteList()
+    var changeHTML = new ChangeHTML(noteList)
+    expect(changeHTML.noteList).isInstanceOf(NoteList)
+  })
+
+  describe('.insertNoteList', () => {
+    it('Inserts our note list into the app index.html file', () => {
+      var noteList = new NoteList()
+      var changeHTML = new ChangeHTML(noteList)
+      changeHTML.insertNoteList();
+      var expectedOutput = '<ul><li><div>Favourite Drink: seltzer</div></li></ul>'
+      expect(document.getElementById("app").innerHTML).includes(expectedOutput)
+    })
+  })
+})
