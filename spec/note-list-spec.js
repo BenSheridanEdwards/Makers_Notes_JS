2ddb06872519edb62ce2a9994541a52081a769a4
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-08 15:40:53 +0000

    Add more tests for functions and change function names

--- spec/note-list-spec.js
+++ spec/note-list-spec.js
@@ -1,16 +1,35 @@
 describe('Note List', () => {
-  describe('.show', () => {
+  describe('.all', () => {
     it('Shows all the notes', () => {
       var noteList = new NoteList()
-      noteList.add("This is my first note")
+      noteList.createNote("This is my first note")
       expect(noteList.all()[0]).isInstanceOf(Note)
     })
   })
+  describe('.createNote', () => {
+    it("creates a note object", () => {
+      var noteList = new NoteList()
+      noteList.createNote("This is my first note")
+      note = noteList.list[0]
+      expect(note).isInstanceOf(Note)
+      expect(note.content()).toEq("This is my first note")
+    })
+    it("adds it to the note list", () => {
+      var noteList = new NoteList()
+      noteList.createNote("This is my first note")
+      expect(noteList.list[0]).isInstanceOf(Note)
+      expect(noteList.list[0].content()).toEq("This is my first note")
+    })
+  })
+  describe('.addNote', () => {
+    it('adds a note object to the note list', () => {
+      var noteList = new NoteList()
+      var note = new Note("This is a new note")
+      noteList.addNote(note)
+      expect(noteList.all()).includes(note)
+    })
+  })
 })
-
-
-
-
 
 
 // (function() {
