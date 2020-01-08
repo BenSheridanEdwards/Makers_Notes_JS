0c469e6735571771a7587ae88f72f2b6c7f1baa8
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-08 15:41:47 +0000

    function name change across app

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -2,8 +2,8 @@ describe('View Notes List', () => {
   describe('.toHTML', () => {
     it('Converts and outputs notes to html strings', () => {
       var noteList = new NoteList()
-      noteList.add('Favourite Food: Chicken Katsu Curry')
-      noteList.add('Favourite Drink: Chocolate Milkshake')
+      noteList.createNote('Favourite Food: Chicken Katsu Curry')
+      noteList.createNote('Favourite Drink: Chocolate Milkshake')
       var noteListView = new NoteListView(noteList)
 
       var expectedOutput = '<ul><li><div>Favourite Food: Chicken Katsu Curry</div></li><li><div>Favourite Drink: Chocolate Milkshake</div></li></ul>'
