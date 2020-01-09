d27c6d28095ec2c25ce7dd190a7d54dc48c4b4ba
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-09 14:19:44 +0000

    All tests passing

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -6,7 +6,7 @@ describe('View Notes List', () => {
       noteList.createNote('Favourite Drink: Chocolate Milkshake')
       var noteListView = new NoteListView(noteList)
 
-      var expectedOutput = '<ul><li><div>Favourite Food: Chic</div></li><li><div>Favourite Drink: Cho</div></li></ul>'
+      var expectedOutput = "<ul><li><div id='0'><a href='#note/0'>Favourite Food: Chic</a></div></li> <li><div id='1'><a href='#note/1'>Favourite Drink: Cho</a></div></li></ul>"
       expect(noteListView.toHTML()).toEq(expectedOutput)
     })
   })
--- spec/single-note-view-spec.js
+++ spec/single-note-view-spec.js
@@ -1,9 +1,9 @@
 describe('Single Note View', () => {
   describe('.noteHTML', () => {
-    it('returns a note as HTML', () => {
+    it('returns a note as HTML without tags', () => {
       var note = new Note("Favourite drink: seltzer")
       var singleNoteView = new SingleNoteView(note)
-      expectedOutput = "<div>Favourite drink: seltzer</div>"
+      expectedOutput = "Favourite drink: seltzer"
       expect(singleNoteView.noteHTML()).toEq(expectedOutput)
     })
   })
