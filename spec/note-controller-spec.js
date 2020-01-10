fe1c5e9585085888ced1f8f89435e499c887c672
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-10 14:19:46 +0000

    Full working app

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,62 +1,56 @@
 describe('Note Controller', () => {
+
   it('Instantates with a note list object', () => {
-    var noteList = new NoteList()
-    var changeHTML = new ChangeHTML(noteList)
     expect(changeHTML.noteList).isInstanceOf(NoteList)
   })
 
   it('Instantates with a empty note list object', () => {
-    var noteList = new NoteList()
-    var changeHTML = new ChangeHTML(noteList)
-    list = changeHTML.noteList.list
-    expect(list).toBeEmpty(list)
+      list = changeHTML.noteList.list
+      expect(list).toBeEmpty(list)
   })
-  
+
   describe('.insertNoteList', () => {
     it('Inserts our note list into the app index.html file', () => {
-      var noteList = new NoteList()
-      noteList.createNote('Favourite Drink: seltzer')
-      var changeHTML = new ChangeHTML(noteList)
+      noteList.createNote('Favourite Drink: White Monster')
+      noteList.createNote('Favourite Drink: Coca-Cola')
       changeHTML.insertNoteList();
-      var expectedOutput = '<ul><li><div id="0"><a href="#note/0">Favourite Drink: sel</a></div></li>'
+      var expectedOutput = '<ul><li><div id="0"><a href="#note/0">Favourite Drink: Whi</a></div></li>'
       expect(document.getElementById("app").innerHTML).includes(expectedOutput)
     })
     it('gives each note object a unique ID', () => {
-      var noteList = new NoteList()
-      noteList.createNote('Favourite Drink: seltzer')
-      noteList.createNote('Favourite Drink: fanta')
-      var changeHTML = new ChangeHTML(noteList)
-      changeHTML.insertNoteList();
       appObject = document.getElementById("app").innerHTML
-      expect(appObject).includes("Favourite Drink: sel")
+      expect(appObject).includes("Favourite Drink: Whi")
       expect(appObject).includes('<div id="0">')
     })
     it("gives each note an ID equal to it's note array index", () => {
-      var noteList = new NoteList()
-      var note = new Note('Favourite Drink: seltzer')
-      var note2 = new Note('Favourite Drink: fanta')
-      noteList.addNote(note)
-      noteList.addNote(note2)
-      var changeHTML = new ChangeHTML(noteList)
-      changeHTML.insertNoteList();
-      expect(noteList.list[0].content()).toEq("Favourite Drink: seltzer")
+      expect(noteList.list[0].content()).toEq("Favourite Drink: White Monster")
       expect(noteList.list[0].content()).includes(document.getElementById("0").innerText)
-      expect(noteList.list[1].content()).toEq("Favourite Drink: fanta")
+      expect(noteList.list[1].content()).toEq("Favourite Drink: Coca-Cola")
       expect(noteList.list[1].content()).includes(document.getElementById("1").innerText)
     })
     it("gives each note a unique URL that matches it's ID", () => {
-      var noteList = new NoteList()
-      var note = new Note('Favourite Drink: seltzer')
-      var note2 = new Note('Favourite Drink: fanta')
-      noteList.addNote(note)
-      noteList.addNote(note2)
-      var changeHTML = new ChangeHTML(noteList)
-      changeHTML.insertNoteList();
       expect(document.getElementById("0").innerHTML).includes('<a href="#note/0">')
       expect(document.getElementById("1").innerHTML).includes('<a href="#note/1">')
     })
   })
  
+  describe('.createNoteFromFormEvent', () => {
+    changeHTML.createNoteFromFormEvent('Event Note')
+
+    it('creates a new note with the text from the submit event', () => {
+      expect(changeHTML.noteList.list[0]).isInstanceOf(Note)
+    })
+    it('refreshes the noteList, displaying the new note', () => {
+      expect(document.getElementById("app").innerHTML).includes('Event Note')
+    })
+  })
+  describe('.changeAppContent', () => {
+    it('loads full note content based on the URL', () => {
+      changeHTML.changeAppContent()
+      expect(document.getElementById("full-note").innerText).includes("Favourite Drink: White Monster")
+    })
+  })
+
   // describe('.listenForUrlChange', () => {
   //   it('listens for a url change event', () => {
     
@@ -75,31 +69,5 @@ describe('Note Controller', () => {
     
   //   })
   // })
-
-  describe('.createNoteFromFormEvent', () => {
-    it('creates a new note with the text from the submit event', () => {
-      var noteList = new NoteList()
-      var changeHTML = new ChangeHTML(noteList)
-      changeHTML.createNoteFromFormEvent('This is my event note')
-      expect(changeHTML.noteList.list[0]).isInstanceOf(Note)
-    })
-    it('refreshes the noteList, displaying the new note', () => {
-      var noteList = new NoteList()
-      var changeHTML = new ChangeHTML(noteList)
-      changeHTML.createNoteFromFormEvent('This is my event note')
-      expect(document.getElementById("app").innerHTML).includes('This is my')
-    })
-  })
-  describe('.changeAppContent', () => {
-    it('loads full note content based on the URL', () => {
-      var noteList = new NoteList()
-      noteList.createNote('Favourite Drink: seltzer')
-      noteList.createNote('Favourite Drink: yellow')
-      var changeHTML = new ChangeHTML(noteList)
-      changeHTML.insertNoteList();
-      changeHTML.changeAppContent('http://127.0.0.1:5500/spec.html#note/0');
-      expect(document.getElementById("full-note").innerText).includes("Favourite Drink: sel")
-    })
-  })
 })
 
--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,13 +1,12 @@
 describe('View Notes List', () => {
   describe('.toHTML', () => {
     it('Converts each note in the note list to html strings', () => {
-      var noteList = new NoteList()
-      noteList.createNote('Favourite Food: Chicken Katsu Curry')
-      noteList.createNote('Favourite Drink: Chocolate Milkshake')
-      var noteListView = new NoteListView(noteList)
-
+      var noteListTest = new NoteList()
+      noteListTest.createNote('Favourite Food: Chicken Katsu Curry')
+      noteListTest.createNote('Favourite Drink: Chocolate Milkshake')
+      var noteListView = new NoteListView()
       var expectedOutput = "<ul><li><div id='0'><a href='#note/0'>Favourite Food: Chic</a></div></li> <li><div id='1'><a href='#note/1'>Favourite Drink: Cho</a></div></li></ul>"
-      expect(noteListView.toHTML()).toEq(expectedOutput)
+      expect(noteListView.toHTML(noteListTest)).toEq(expectedOutput)
     })
   })
 })
